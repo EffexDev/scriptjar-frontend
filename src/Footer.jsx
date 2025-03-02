493717047d1d466207ae7c760d81ef2a6845c2ae
EffexDev <effexdev@gmail.com>
2025-03-03 00:36:16 +0800

    fixed footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,12 +8,12 @@ function Footer() {
 
     return (
         <div className="absolute bottom-0 left-0 w-full flex flex-col items-center justify-center py-2 bg-blue-600">
-            <p className="text-white">© {copyrightYear} Jordan Cartledge. All rights reserved.</p>
             <div className="flex flex-row text-white w-25 justify-evenly">
                 <a href="https://x.com/EffexForge" target="_blank"><XIcon /></a>
                 <a href="https://www.instagram.com/thesmoothdescent/?next=%2Feffexforge%2F" target="_blank"><InstagramIcon /></a>
                 <a href="https://github.com/EffexDev" target="_blank" className="pl-1"><GitHubIcon /></a>
             </div>
+            <p className="text-white pt-2">© {copyrightYear} Jordan Cartledge. All rights reserved.</p>
         </div>
     );
 }
