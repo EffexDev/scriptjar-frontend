628ce42552ba0f727afa9d3600fee9227c60358f
EffexDev <jtcartled@gmail.com>
2025-03-03 12:09:41 +0800

    Removed API Keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHDGsIjjTj3zRWoWvy7ZuA3Q1v6oWENQY",
-  authDomain: "scriptjar-418ae.firebaseapp.com",
-  projectId: "scriptjar-418ae",
-  storageBucket: "scriptjar-418ae.firebasestorage.app",
-  messagingSenderId: "877989180382",
-  appId: "1:877989180382:web:355c297a5c593a250d234f"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
