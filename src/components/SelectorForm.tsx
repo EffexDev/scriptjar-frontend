8e4065b8544d327c130bddfb188e02f8d32d10fb
Effex <jordan@effexforge.com>
2025-03-06 21:47:39 +0800

    Update SelectorForm.tsx

--- src/components/SelectorForm.tsx
+++ src/components/SelectorForm.tsx
@@ -4,8 +4,8 @@ import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 function SelectorForm() {
     return (
         <div className="rounded-lg bg-gray-100/20 backdrop-blur-6xl p-10 text-white">
-            <FormControl sx={{ m: 1, minWidth: 120, color: "white" }} size="small">
-            <InputLabel id="demo-select-small-label">Age</InputLabel>
+            <FormControl sx={{ m: 1, minWidth: 120}} size="small">
+            <InputLabel sx={{color: "white"}} id="demo-select-small-label">Age</InputLabel>
                 <Select
                     labelId="demo-select-small-label"
                     id="demo-select-small"
