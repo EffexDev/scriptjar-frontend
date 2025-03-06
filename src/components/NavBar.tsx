b909497b62c1d75d8707faa03dce18e7e3b089db
Effex <jordan@effexforge.com>
2025-03-06 22:15:41 +0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 function NavBar() {
     return (
-        <div className="pl-10 w-full flex flex-col sm:flex-row items-center sm:justify-between pt-10">
+        <div className="w-full flex flex-col sm:flex-row items-center sm:justify-between pt-10">
             <Header />
             <LogoutButton />
         </div>
